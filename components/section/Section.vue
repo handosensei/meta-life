683c7369dc8d6636a06d2797b2d557e6dbee5414
Ruud Luijten <ruud@rlly.co>
2022-03-12 08:43:53 +0100

    Create heading component (#22)

--- components/section/Section.vue
+++ components/section/Section.vue
@@ -4,12 +4,18 @@
     :class="{ 'isLight': theme === 'light' }"
   >
     <!-- TODO: Remove this -->
-    <h1 class="title">{{ name }}</h1>
+    <h1 class="head">{{ name }}</h1>
 
     <SectionText
+      v-if="text"
       :has-play-trailer-button="hasPlayTrailerButton"
       :number="chapterNumber"
       :text="text"
+    />
+
+    <SectionTitle
+      v-if="title"
+      :title="title"
     />
   </section>
 </template>
@@ -20,12 +26,14 @@ import { mapState } from 'vuex';
 import { getChapterIndex } from '~/utils/functions/chapterHelpers';
 
 import SectionText from '~/components/text/Text.vue';
+import SectionTitle from '~/components/title/Title.vue';
 
 export default {
   name: 'SectionComponent',
 
   components: {
     SectionText,
+    SectionTitle,
   },
 
   props: {
@@ -46,6 +54,12 @@ export default {
     },
 
     text: {
+      type: String,
+      required: false,
+      default: '',
+    },
+
+    title: {
       type: String,
       required: false,
       default: '',
---
+++ components/title/Title.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2 class="title">{{ title }}</h2>
+</template>
+
+<script>
+export default {
+  name: 'TitleComponent',
+
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './Title';
+</style>
