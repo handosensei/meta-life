dd8feb1471330876e13cae1282f2b97b5132c65d
Ruud Luijten <ruud@rlly.co>
2022-03-23 14:19:18 +0100

    Update heading props

--- components/heading/Heading.vue
+++ components/heading/Heading.vue
@@ -1,5 +1,14 @@
 <template>
-  <h2 class="heading">{{ title }}</h2>
+  <div>
+    <h2
+      class="heading"
+      :class="{ 'isLarge': size === 'large' }"
+    >
+      {{ title }}
+    </h2>
+
+    <NuxtLink v-if="link" :to="link.href">{{ link.text }}</NuxtLink>
+  </div>
 </template>
 
 <script>
@@ -7,9 +16,22 @@ export default {
   name: 'HeadingComponent',
 
   props: {
+    size: {
+      type: String,
+      required: false,
+      default: 'small',
+      validator: (value) => ['small', 'large'].includes(value),
+    },
+
     title: {
       type: String,
       required: true,
+    },
+
+    link: {
+      type: Object,
+      required: false,
+      default: () => {}
     },
   }
 }
