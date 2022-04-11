9756e01a38e80316d042b255198c0364db1028e7
Rob Smittenaar <info@robsmittenaar.com>
2022-04-11 13:20:29 +0200

    feat(backToExperience): sticky

--- components/backToExperience/BackToExperience.vue
+++ components/backToExperience/BackToExperience.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="backToExperience" :class="{ 'isLight': theme === 'light' }">
-    <NuxtLink to="/" class="link">Back to the Experience</NuxtLink>
-    <Icon type="LogoOutline" class="logoOutline" />
+    <div ref="container" class="container">
+      <NuxtLink to="/" class="link">
+        <span class="link-line">Back to the</span>
+        <span class="link-line">Experience</span>
+      </NuxtLink>
+      <Icon type="LogoOutline" class="logoOutline" />
+    </div>
   </div>
 </template>
 
 <script>
+import { gsap } from 'gsap';
+import { mapState } from 'vuex';
+
+import { inview } from '~/utils/functions/inview';
+import { progress } from '~/utils/functions/math';
+
 import Icon from '~/components/elements/Icon.vue';
 
 export default {
@@ -21,7 +32,56 @@ export default {
       required: false,
       default: 'dark',
     }
-  }
+  },
+
+  computed: {
+    ...mapState('app', ['windowSize']),
+  },
+
+  mounted() {
+    this.$nuxt.$on('window:raf', this.onRaf);
+    this.$nuxt.$on('window:resize', this.onResize);
+
+    this.setAnimation();
+  },
+
+  beforeDestroy() {
+    this.$nuxt.$off('window:raf', this.onRaf);
+    this.$nuxt.$off('window:resize', this.onResize);
+  },
+
+  methods: {
+    onRaf() {
+      if (inview(this.$el, this.windowSize.height, 0)) {
+        this.setProgress();
+      }
+    },
+
+    onResize() {
+      this.setAnimation();
+    },
+
+    setProgress() {
+      const bounds = this.$el.getBoundingClientRect();
+
+      const value = bounds.top - this.windowSize.height;
+      const max = bounds.height;
+      
+      this.progress = progress(value, 0, max);
+      this.scrollTl.progress(-this.progress);
+    },
+
+    setAnimation() {
+      const bounds = this.$el.getBoundingClientRect();
+      
+      this.scrollTl = gsap.timeline({ paused: true });
+      this.scrollTl
+      .fromTo(this.$refs.container,
+        { y: -bounds.height / 2 },
+        { y: 0, ease: 'linear' }
+      );
+    },
+  },
 }
 </script>
 
---
+++ utils/functions/math.js
@@ -0,0 +1,1 @@
+export const progress = (val, min, max) => val / (max - min) + min;
