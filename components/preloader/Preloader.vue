23ea5f96e08d220360e54fe0597c5ca49b45501d
Rob Smittenaar <info@robsmittenaar.com>
2022-04-18 11:16:39 +0200

    feat(preloader): animation

--- components/preloader/Preloader.vue
+++ components/preloader/Preloader.vue
@@ -1,24 +1,25 @@
 <template>
   <div class="preloader">
-    <Icon type="PreloaderLogo" class="preloaderLogo" />
+    <Icon ref="preloaderLogo" type="PreloaderLogo" class="preloaderLogo" />
 
-    <p class="title">
+    <p ref="title" class="title">
       Enter in the new world
     </p>
     
-    <Icon type="wordmark" class="wordmark" />
+    <Icon ref="wordmark" type="wordmark" class="wordmark" />
 
     <div class="progress">
       <div ref="progressCount" class="progressCount">0%</div>
-      <Icon type="PreloaderProgress" class="preloaderProgress" />
+      <Icon ref="preloaderProgress" type="PreloaderProgress" class="preloaderProgress" />
     </div>
 
-    <Icon type="LogoOutline" class="logoOutline" />
+    <Icon ref="logoOutline" type="LogoOutline" class="logoOutline" />
   </div>
 </template>
 
 <script>
 import { gsap } from 'gsap';
+import { SplitText } from 'gsap/SplitText';
 import { mapActions, mapState } from 'vuex';
 
 import homeData from '~/content/home.json';
@@ -50,6 +51,8 @@ export default {
   },
 
   mounted() {
+    this.initTl();
+
     this.preloadGalleryThumbs();
 
     if (!this.introSkipped) {
@@ -73,6 +76,24 @@ export default {
   },
 
   methods: {
+    initTl() {
+      this.splitSubtitle = new SplitText(
+        this.$refs.title,
+        {
+          type: 'chars',
+          charsClass: 'subtitle-char',
+        }
+      );
+
+      this.wordmark = this.$refs.wordmark.$el;
+      this.wordmarChars = Array.prototype.slice.call(this.wordmark.children);
+
+      this.logoOutline = this.$refs.logoOutline.$el;
+
+      this.preloaderLogo = this.$refs.preloaderLogo.$el;
+      this.preloaderProgress = this.$refs.preloaderProgress.$el;
+    },
+    
     preloadGalleryThumbs() {
       // Preload gallery thumbnails
       const galleryThumbs = homeData.chapters
@@ -89,12 +110,53 @@ export default {
     },
 
     enter() {
-      
+      const enterTl = gsap.timeline({ defaults: { ease: 'expo.out' } });
+
+      const from = `polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)`;
+      const to = `polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%`;
+
+      enterTl
+        .fromTo(
+          this.logoOutline.children,
+          { drawSVG: '0%' },
+          { drawSVG: '100%', duration: 2, delay: 1, ease: 'linear' }, 0
+        )
+        .fromTo(
+          this.preloaderLogo.children[0],
+          { drawSVG: '0%' },
+          { drawSVG: '100%', duration: 1, delay: 1 }, 0
+        )
+        .fromTo(
+          this.preloaderLogo.children,
+          { y: 10, autoAlpha: 0 },
+          { y: 0, autoAlpha: 1, duration: 1, delay: 0.5, stagger: { from: 'edges', amount: 0.1 } }, 0
+        )
+        .fromTo(
+          this.wordmarChars.slice(0, 4),
+          { yPercent: -120 },
+          { yPercent: 0, duration: 1, delay: 0.5, stagger: { from: 'start', amount: 0.5 } }, 0
+        )
+        .fromTo(
+          this.wordmarChars.slice(4, 8),
+          { yPercent: 120 },
+          { yPercent: 0, duration: 1, delay: 0.5, stagger: { from: 'start', amount: 0.5 } }, 0
+        )
+        .fromTo(
+          this.preloaderProgress.children[0],
+          { clipPath: from },
+          { clipPath: to, duration: 2, ease: 'linear' }, 0
+        )
+        .fromTo(
+          this.splitSubtitle.chars,
+          { autoAlpha: 0 },
+          { autoAlpha: 1, duration: 1, stagger: { from: 'edges', amount: 0.5 } }, 0
+        );
     },
 
     leave(done) {
       this.setPreloaderVisible(false);
-      done();
+
+      gsap.to(this.$el, { autoAlpha: 0, onComplete: done, delay: 0.5 } );
     },
 
     ...mapActions('app', ['setAssetsPreloaded', 'setPreloaderVisible']),
