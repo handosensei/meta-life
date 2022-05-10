29fba8ba20b986c6eb5114ba22761e56d1f69185
felix-geffrault <58772408+felix-geffrault@users.noreply.github.com>
2022-05-10 15:49:25 +0200

    Contact Page

---
+++ components/contact/Contact.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="contact">
+    <div class="main">
+        <h1>Contact</h1>
+        <div class="infos">
+          <p>Press : <a class="email">press@metalife.com</a><br/>
+          Infos : <a class="email">contact@metalife.com</a><br/>
+          <br/>
+          Questions about economic ecosystem, read our whitepaper sections.
+          </p>
+        </div>
+    </div>
+    <div class="socials">
+      <h2>Official Channel</h2>
+      <ul>
+        <li><a href="#">Discord</a></li>
+        <li><a href="#">Twitter</a></li>
+        <li><a href="#">Instagram</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ContactComponent',
+};
+</script>
+
+<style lang="scss" scoped>
+@import './Contact';
+</style>
---
+++ pages/contact/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <main class="page">
+    <div ref="bounding" class="bounding">
+      <Contact />
+    </div>
+  </main>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+import Debug from '~/mixins/debug';
+import PageTransition from '~/mixins/pageTransition';
+import Scroll from '~/mixins/scroll';
+import Contact from '~/components/contact/Contact.vue'
+
+export default {
+  name: 'ContactPage',
+
+  mixins: [Debug, PageTransition, Scroll],
+
+  components: {
+    Contact,
+  },
+
+  beforeMount() {
+    this.setTheme('dark');
+  },
+
+  methods: {
+    ...mapActions('app', ['setTheme']),
+  },
+};
+</script>
