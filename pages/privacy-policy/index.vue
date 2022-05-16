b6879f32e28f79dde7c9ae78caad052ae1eba7b9
felix-geffrault <58772408+felix-geffrault@users.noreply.github.com>
2022-05-16 12:13:09 +0200

    Privacy Policy Page

---
+++ pages/privacy-policy/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <main class="page">
+    <div ref="bounding" class="bounding">
+      <PrivacyPolicy data-section />
+      <BackToExperience data-section /> 
+    </div>
+  </main>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+import Debug from '~/mixins/debug';
+import PageTransition from '~/mixins/pageTransition';
+import Scroll from '~/mixins/scroll';
+
+import BackToExperience from '~/components/backToExperience/BackToExperience.vue';
+import PrivacyPolicy from '~/components/privacy-policy/PrivacyPolicy.vue'
+
+export default {
+  name: 'PrivacyPolicyPage',
+  
+  components: {
+    PrivacyPolicy,
+    BackToExperience
+  },
+
+  mixins: [Debug, PageTransition, Scroll],
+
+  beforeMount() {
+    this.setTheme('dark');
+  },
+
+  methods: {
+    ...mapActions('app', ['setTheme']),
+  },
+};
+</script>
