f671bc969db0891437320f0096441d2d34eb75bf
Baptiste Briel <baptiste@antinomy.studio>
2022-05-27 17:49:19 +0200

    hide fps for now

--- plugins/global.js
+++ plugins/global.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import Stats from 'stats.js';
+// import Stats from 'stats.js';
 
 import Performances from '~/utils/config/Performances';
 
@@ -9,16 +9,16 @@ export default async function ({ app }, inject) {
   console.log('%c✦ by Antinomy Studio', styles);
   console.log('https://antinomy.studio');
 
-  if (process.env.NODE_ENV === 'development') {
-    const stats = new Stats();
-    Object.assign(stats.dom.style, {
-      top: 'initial',
-      bottom: 0,
-      zIndex: 99999,
-    });
-    document.body.appendChild(stats.dom);
-    inject('stats', stats);
-  }
+  // if (process.env.NODE_ENV === 'development') {
+  //   const stats = new Stats();
+  //   Object.assign(stats.dom.style, {
+  //     top: 'initial',
+  //     bottom: 0,
+  //     zIndex: 99999,
+  //   });
+  //   document.body.appendChild(stats.dom);
+  //   inject('stats', stats);
+  // }
 
   await Performances.getPerfs();
 
