3bd2148a4de421e7e014ac2120b0b7303975b44c
Baptiste Briel <baptiste@antinomy.studio>
2022-06-07 10:30:03 +0200

    fixes #105

--- components/newsGrid/NewsGrid.vue
+++ components/newsGrid/NewsGrid.vue
@@ -2,9 +2,21 @@
   <ul class="newsGrid">
     <li v-for="item in items" :key="item.slug" class="item">
       <NuxtLink class="link" :to="`/news/${item.slug}`">
-        <img :src="item.image.src" class="thumb" :alt="item.image.alt" />
-        <div class="category">{{ item.articleType }}</div>
-        <div class="title">{{ item.title }}</div>
+        <div class="thumb">
+          <img :src="item.image.src" :alt="item.image.alt" />
+        </div>
+        <div class="row">
+          <div>
+            <div class="category">{{ item.articleType }}</div>
+            <div class="title">{{ item.title }}</div>
+          </div>
+          <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g>
+              <line y1="29.5" x2="60" y2="29.5" stroke="white"/>
+              <line x1="30.5" y1="2.18557e-08" x2="30.5" y2="60" stroke="white"/>
+            </g>
+          </svg>
+        </div>
       </NuxtLink>
     </li>
   </ul>
