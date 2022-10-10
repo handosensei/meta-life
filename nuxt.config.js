2b8e35b8e30f0111d451a2bd744f36bb1edd4de3
Blyzi <flash.spaces@gmail.com>
2022-10-10 05:28:09 -0700

    fix: final

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
     htmlAttrs: {
       lang: 'en',
     },
-    title:'Meta Life : The metaverse of tomorrow',
+    title:'Meta Life - The metaverse of tomorrow',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
