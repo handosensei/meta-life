818df15b864cd232ae4e0e051a3400d92c09b814
Blyzi <flash.spaces@gmail.com>
2022-10-17 00:13:58 -0700

    fix: content/credit

--- plugins/global.js
+++ plugins/global.js
@@ -4,10 +4,14 @@
 import Performances from '~/utils/config/Performances';
 
 export default async function ({ app }, inject) {
-  const styles = ['color: black', 'font-size: 14px', 'line-height: 10px', 'padding: 10px 0'].join(';');
+  const antinomyStyles = ['color: black', 'font-size: 14px', 'line-height: 10px', 'padding: 10px 0'].join(';');
+  const theoStyle = ['color: #10b981', 'font-size: 14px', 'line-height: 10px', 'padding: 10px 0'].join(';');
 
-  console.log('%c✦ by Antinomy Studio', styles);
+  console.log('%c✦ by Antinomy Studio', antinomyStyles);
   console.log('https://antinomy.studio');
+
+  console.log('%c✦ by Theo Lasnier', theoStyle);
+  console.log('https://theo-lasnier.dev');
 
   // if (process.env.NODE_ENV === 'development') {
   //   const stats = new Stats();
